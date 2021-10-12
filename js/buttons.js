344457e415bd6bc58ba56cdbe7a7de4f7a4099e2
Allan Kong <allankong176@gmail.com>
2021-10-11 18:59:02 -0600

    added main changes

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,9 @@
 var Advocacy = false;
 var Education = false;
+var Events = false;
+var Opportunities = false;
+var Team = false;
+var Sponsors = false;
 
 function expandAdvocacy() {
     var text = "<ul><li>We work with community groups to understand and address the barriers faced by underserved populations to STEM educational programming and other opportunities.</li><li>We initiate projects that focus on improving the accessibility, and the relevance and design of these programs and opportunities.</li><li> We collaborate with educational institutions on projects that aim to create systemic change.</li></ul>";
@@ -45,8 +49,8 @@ function expandEvents() {
     var button = document.getElementById("eventsbutton").classList;
     var ClosedContent = document.getElementById("eventstext");
 
-    Education = !Education;
-    if(Education)
+    Events = !Events;
+    if(Events)
     {
         button.add("IO-button-active");
         OpenContent.remove("IO-content-closed");
@@ -63,8 +67,46 @@ function expandOpportunities() {
     var button = document.getElementById("opportunitiesbutton").classList;
     var ClosedContent = document.getElementById("opportunitiestext");
 
-    Education = !Education;
-    if(Education)
+    Opportunities = !Opportunities;
+    if(Opportunities)
+    {
+        button.add("IO-button-active");
+        OpenContent.remove("IO-content-closed");
+        ClosedContent.innerText = "";
+        return;
+    }
+    ClosedContent.innerHTML = text;
+    OpenContent.add("IO-content-closed");
+    button.remove("IO-button-active");
+}
+
+function expandExecs() {
+    var text = "Free computer programming education for children from low-income households, including workshops, one-on-one mentorship, and coding challenges for prizes.\n"
+    var OpenContent = document.getElementById("execscontent").classList;
+    var button = document.getElementById("execsbutton").classList;
+    var ClosedContent = document.getElementById("execstext");
+
+    Team = !Team;
+    if(Team)
+    {
+        button.add("IO-button-active");
+        OpenContent.remove("IO-content-closed");
+        ClosedContent.innerText = "";
+        return;
+    }
+    ClosedContent.innerHTML = text;
+    OpenContent.add("IO-content-closed");
+    button.remove("IO-button-active");
+}
+
+function expandSponsors() {
+    var text = "Free computer programming education for children from low-income households, including workshops, one-on-one mentorship, and coding challenges for prizes.\n"
+    var OpenContent = document.getElementById("sponsorscontent").classList;
+    var button = document.getElementById("sponsorsbutton").classList;
+    var ClosedContent = document.getElementById("sponsorstext");
+
+    Sponsors = !Sponsors;
+    if(Sponsors)
     {
         button.add("IO-button-active");
         OpenContent.remove("IO-content-closed");
